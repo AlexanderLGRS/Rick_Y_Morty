8d585f71975d646d4451bc5bde37df1ff93520b8
AlexLGRS <alexlansdorprs@gmail.com>
2022-01-20 14:50:28 -0500

    full

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from 'react';
+import './App.css'
+let list
+const arrow = '../next.png'
 function App() {
+
+  const [characterName, setCharacterName] = useState('')
+  const [characterIndex, setCharacterIndex] = useState(0)
+  const [characterImg, setCharacterImg] = useState('')
+  const [characterStatus, setCharacterStatus] = useState('')
+  const [characterSpecies, setCharacterSpecies] = useState('')
+
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character/?page=19',
+    )
+      .then(response => response.json())
+      .then(data => {
+        list = data.results.length - 1
+        console.log(characterIndex);
+        setCharacterImg(data.results[characterIndex].image)
+        setCharacterName(data.results[characterIndex].name)
+        setCharacterSpecies(data.results[characterIndex].species)
+        setCharacterStatus(data.results[characterIndex].status)
+        document.title = `R&M || ${data.results[characterIndex].name}`;
+      });
+  }, [characterIndex])
+
+  const increase = () => {
+    if (characterIndex < list) {
+      setCharacterIndex(characterIndex + 1)
+    }
+  }
+  const decrease = () => {
+    if (characterIndex > 0) {
+      setCharacterIndex(characterIndex - 1)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="buttons-container">
+        <button type="button" class="boton btn btn-primary" onClick={decrease}>
+        <img className='prev' src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/000000/external-left-arrow-miscellaneous-kiranshastry-solid-kiranshastry.png"/>
+        </button>
+        <button type="button" class="boton btn btn-primary" onClick={increase}>
+        <img className='next' src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/000000/external-left-arrow-miscellaneous-kiranshastry-solid-kiranshastry.png"/>
+        </button>
+      </div>
+      <div className="card">
+        <h2 className='character-name' >{characterName}</h2>
+        <img className='character-img' src={characterImg} alt="" />
+        <p className='badge bg-success text-dark'>{characterSpecies}</p>
+        <p className='badge bg-warning text-dark'>{characterStatus}</p>
+      </div>
+    </React.Fragment >
   );
 }
 
 export default App;
+
